191927f10ab1e9a86fcdff15a4072d5d5f70bce2
Neeshu Sharma <114111237+NeeshuSharmaa@users.noreply.github.com>
2023-09-01 14:42:34 +0530

    built: sidebar, navbar & home summary UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { Route, Routes } from "react-router";
+import Home from "./pages/home/Home";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Timer from "./pages/timer/Timer";
 import "./App.css";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
-  return <></>;
+  return (
+    <div className="app">
+      <nav className="logo">
+        <img src="F.png" alt="logo" />
+        <h2>Focus Flow</h2>
+      </nav>
+      <div className="main">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/insights" element={<Dashboard />} />
+          <Route path="/timer" element={<Timer />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,54 @@
+import "./Home.css";
+import { created } from "../../features/createTaskSlice";
+import { useDispatch } from "react-redux";
+
+export default function Home() {
+  const dispatch = useDispatch();
+  return (
+    <div className="home">
+      <section className="summary">
+        <div className="summary-child">
+          <div className="summary-child-main">
+            <h1>0</h1>
+            <small>m</small>
+          </div>
+          <small>Estimated Time</small>
+        </div>
+        <div className="summary-child">
+          <h1>0</h1>
+
+          <small>Tasks to be Completed</small>
+        </div>
+        <div className="summary-child">
+          <div className="summary-child-main">
+            <h1>0</h1>
+            <small>m</small>
+          </div>
+          <small>Elapsed Time</small>
+        </div>
+        <div className="summary-child">
+          <h1>0</h1>
+          <small>Completed Tasks</small>
+        </div>
+      </section>
+      <button>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-6 h-6"
+          height="25"
+          width="25"
+        >
+          <path
+            fillRule="evenodd"
+            d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z"
+            clipRule="evenodd"
+          />
+        </svg>
+        <span>Track New Task</span>
+      </button>
+      <section className="tasks-list"></section>
+    </div>
+  );
+}
