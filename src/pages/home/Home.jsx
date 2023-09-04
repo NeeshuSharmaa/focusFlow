8a17b8b4c4ba458ea310c10e5b5d27118390b8db
Neeshu Sharma <114111237+NeeshuSharmaa@users.noreply.github.com>
2023-09-04 15:28:57 +0530

    styled, refactored, rendered tasks on home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,39 +3,18 @@ import "./Home.css";
 
 import TaskModal from "../../components/taskModal/TaskModal";
 import { useSelector } from "react-redux";
+import SummaryOnHome from "./SummaryOnHome";
+import Task from "../../components/task/Task";
 
 export default function Home() {
   const [showTaskModal, setShowTaskModal] = useState(false);
+
   const tasks = useSelector((state) => state.tasks.allTasks);
-  console.log(tasks);
+
   return (
     <div className="home">
-      <section className="summary">
-        <div className="summary-child">
-          <div className="summary-child-main">
-            <h1>0</h1>
-            <small>m</small>
-          </div>
-          <small>Estimated Time</small>
-        </div>
-        <div className="summary-child">
-          <h1>0</h1>
-
-          <small>Tasks to be Completed</small>
-        </div>
-        <div className="summary-child">
-          <div className="summary-child-main">
-            <h1>0</h1>
-            <small>m</small>
-          </div>
-          <small>Elapsed Time</small>
-        </div>
-        <div className="summary-child">
-          <h1>0</h1>
-          <small>Completed Tasks</small>
-        </div>
-      </section>
-      <button onClick={() => setShowTaskModal(true)}>
+      <SummaryOnHome tasks={tasks} />
+      <button className="primary-btn" onClick={() => setShowTaskModal(true)}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
@@ -46,17 +25,30 @@ export default function Home() {
         >
           <path
             fillRule="evenodd"
-            d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z"
+            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 9a.75.75 0 00-1.5 0v2.25H9a.75.75 0 000 1.5h2.25V15a.75.75 0 001.5 0v-2.25H15a.75.75 0 000-1.5h-2.25V9z"
             clipRule="evenodd"
           />
         </svg>
-        <span>Track New Task</span>
+
+        <span>Create New Task</span>
       </button>
-      <section className="tasks-list">
-        {tasks.map((task) => (
-          <span key={task.id}>{task.name}</span>
-        ))}
-      </section>
+
+      {!!tasks.length && (
+        <section className="tasks-list">
+          {tasks.map((task) => (
+            <Task key={task.id} {...task} />
+          ))}{" "}
+        </section>
+      )}
+      {tasks.length === 0 && (
+        <div className="no-tasks">
+          <img src="no-tasks.png" alt="no-tasks-illustration" />
+          <div>
+            <span>No tasks</span>
+            <small>{`Click the "Create New Task" button to add`}</small>
+          </div>
+        </div>
+      )}
       <TaskModal
         setShowTaskModal={setShowTaskModal}
         showTaskModal={showTaskModal}
---
+++ src/pages/home/SummaryOnHome.jsx
@@ -0,0 +1,22 @@
+export default function SummaryOnHome() {
+  return (
+    <section className="summary">
+      <div className="summary-child">
+        <h1>0</h1>
+
+        <small>Tasks to be Completed</small>
+      </div>
+      <div className="summary-child">
+        <div className="summary-child-main">
+          <h1>0</h1>
+          <small>m</small>
+        </div>
+        <small>Elapsed Time</small>
+      </div>
+      <div className="summary-child">
+        <h1>0</h1>
+        <small>Completed Tasks</small>
+      </div>
+    </section>
+  );
+}
