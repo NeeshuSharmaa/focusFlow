c83a3d3a953807186088758308e789b9f887337a
Neeshu Sharma <114111237+NeeshuSharmaa@users.noreply.github.com>
2023-09-09 15:19:10 +0530

    added: filter reducer in redux store

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import tasksReducer from "../features/tasksSlice";
+import filtersReducer from "../features/filterSlice";
 
 // all the reducers will be combined using configureStore
 const store = configureStore({
   reducer: {
     tasks: tasksReducer,
+    filters: filtersReducer,
   },
 });
 
